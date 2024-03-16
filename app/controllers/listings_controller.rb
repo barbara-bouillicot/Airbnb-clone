8a19a6f4f133670da889e1b7435daf568b23b366
barbara bouillicot <barbara.bouillicot@hotmail.fr>
2024-03-16 12:02:42 +0100

    Listing controller added

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,26 @@
 class ListingsController < ApplicationController
+
+ def index
+  @listings = Listing.all
+ end
+
+ def new
+  @listing = Listing.new
+ end
+
+ def create
+  @listing = Listing.new(listing_params)
+  @listing.save
+  if @listing.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+ end
+
+private
+
+ def listing_params
+  params.require(:listing).permit(:name, :address, :availability_from, :availability_to, :maximum_pets, :home_type, :price)
+ end
 end
