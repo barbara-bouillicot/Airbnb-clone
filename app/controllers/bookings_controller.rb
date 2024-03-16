f4a85ee3390f27c77170650508dcb75776e832a2
barbara bouillicot <barbara.bouillicot@hotmail.fr>
2024-03-16 13:30:05 +0100

    bookings controller added and some CRUD actions

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,35 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    @listing = Listing.find(params[:listing_id])
+    @booking.total = @listing.price
+  end
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new(booking_params)
+    @booking.listing = @listing
+    @booking.user = current_user
+    @booking.total = @listing.price
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :listings, only:[ :index, :show, :new, :create, :edit, :update, :destroy]
+  resources :bookings, only: [:index , :show]
+
+  resources :listings, only:[ :index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :bookings, only: [ :new, :create]
+  end
 
 end
